d5ec32358f661fd941c54a9917d1eaafda0d673d
Jimmy Tang <jimmytg1993@gmail.com>
2018-03-01 13:19:07 -0500

    round earth

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 var num = 50;
-<<<<<<< HEAD
-var truth = "Hello Flatter Earth";
-=======
-var truth = "Hello Flat Earth";
->>>>>>> d4901bc18803ee2f8c6d5311f5479d330ab96211
+var truth = "Round Earth"
